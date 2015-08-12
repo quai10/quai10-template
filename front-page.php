5d19d1a239f01fb2bd2e2db3ac4483fdae199044
Damien Senger <hi@hiwelo.co>
2015-08-13 01:01:32 +0200

    Coworkers block o/ homepage

--- front-page.php
+++ front-page.php
@@ -16,6 +16,7 @@
         'menu_id' => 'navigation'
       );
       wp_nav_menu($args);
+      unset($args);
       ?>
     </nav><!-- .nav-box -->
     <div class="header-container grid-1-4">
@@ -35,8 +36,34 @@
       if ($loop->have_posts()) : while ($loop->have_posts()) : $loop->the_post();
     ?>
     <section class="asso-box">
-      <h2 class="asso-title"><?php the_title(); ?></h2>
+      <h2><?php the_title(); ?></h2>
       <?php the_content(); ?>
     </section>
-    <?php endwhile; endif; ?>
+  <?php endwhile; endif; unset($loop, $args); wp_reset_postdata(); ?>
+    <section class="coworkers-box">
+      <h2><?php echo get_post_meta(get_the_ID(), 'coworkers_title', true); ?></h2>
+      <?php
+      $args = array(
+        'category_name' => get_post_meta(get_the_ID(), 'coworkers_category', true),
+        'orderby' => 'rand'
+      );
+      $loop = new WP_Query($args);
+      if ($loop->have_posts()) :
+      ?>
+      <ul class="coworkers-list grid-3">
+        <?php while ($loop->have_posts()) : $loop->the_post(); ?>
+        <li class="coworkers-element">
+          <div class="coworkers-element-image">
+            <?php $url = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()))[0]; ?>
+            <img src="<?php echo $url; ?>" alt="Photo de <?php the_title(); ?>" title="Photo de <?php the_title(); ?>">
+          </div><!-- .coworkers-element-image -->
+          <h3 class="h5-like coworkers-element-name"><?php the_title(); ?></h3>
+          <p class="coworkers-element-work"><?php echo get_post_meta(get_the_ID(), 'work', true); ?></p>
+        </li><!-- .coworkers-element -->
+        <?php unset($url); endwhile; ?>
+      </ul><!-- .coworkers-list -->
+      <?php else : ?>
+        <p>Aucun voyageur n'a pris place dans le train actuellement.</p>
+      <?php endif; unset($loop, $args); wp_reset_postdata(); ?>
+    </section><!-- .coworkers-box -->
   </div><!-- .presentation-box -->
