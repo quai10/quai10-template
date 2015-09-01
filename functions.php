d0b84cee6405b131353d6e1c5883dc2756b9084a
Pierre Rudloff <contact@rudloff.pro>
2015-09-01 02:24:09 +0200

    Lint

--- functions.php
+++ functions.php
@@ -2,12 +2,22 @@
 /**
  * Libraries includes
  * The $includes array determines the code library included in the theme.
- * */
-
+ *
+ * PHP version 5
+ *
+ * @category Template
+ * @package  Quai10
+ * @author   Pierre Rudloff <contact@rudloff.pro>
+ * @author   Damien Senger <hi@hiwelo.co>
+ * @license  GPL http://www.gnu.org/licenses/gpl.html
+ * @link     https://quai10.org/
+ */
 $includes = array(
   'lib/cleanup.php',
   'lib/config.php'
 );
 
-foreach ($includes as $inc) require_once locate_template($inc);
+foreach ($includes as $inc) {
+    include_once locate_template($inc);
+}
 unset($inc);
