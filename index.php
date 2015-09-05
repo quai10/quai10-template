777bdc95acb4093e64463006108098c98d0688ab
Pierre Rudloff <contact@rudloff.pro>
2015-09-05 19:52:55 +0200

    Check if functions exist Lint

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
-get_header();
-get_footer();
+/**
+ * Theme index
+ *
+ * PHP version 5
+ *
+ * @category Template
+ * @package  Quai10
+ * @author   Pierre Rudloff <contact@rudloff.pro>
+ * @author   Damien Senger <hi@hiwelo.co>
+ * @license  GPL http://www.gnu.org/licenses/gpl.html
+ * @link     https://quai10.org/
+ */
+if (function_exists('get_header')) {
+    get_header();
+}
+if (function_exists('get_footer')) {
+    get_footer();
+}
