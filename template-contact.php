8a0de0c0ad9a000b1ac559b830e675c1d4665dd9
Damien Senger <hi@hiwelo.co>
2015-09-05 16:55:43 +0200

    Main content page contact

--- template-contact.php
+++ template-contact.php
@@ -7,9 +7,9 @@ get_header('page');
 <div class="tpl_contact-container container content-box margin-large grid-2 no-padding">
   <section class="tpl_contact-content">
     <?php the_content(); ?>
-    <ul class="tpl_contact-contacts">
-      <li><a href="mailto:<?php echo get_field('contact_email'); ?>"><?php echo get_field('contact_email'); ?></a></li>
-      <li><a href="tel:+33<?php echo substr(implode(explode(' ', get_field('contact_phone'))), 1); ?>"><?php echo get_field('contact_phone'); ?></a></li>
+    <ul class="tpl_contact-contacts" style="background-image: url(<?php echo get_field('contact_background'); ?>)">
+      <li><a class="no-style" href="mailto:<?php echo get_field('contact_email'); ?>"><?php echo get_field('contact_email'); ?></a></li>
+      <li><a class="no-style" href="tel:+33<?php echo substr(implode(explode(' ', get_field('contact_phone'))), 1); ?>"><?php echo get_field('contact_phone'); ?></a></li>
     </ul><!-- .tpl_contact-contacts -->
   </section><!-- .tpl_contact-content -->
   <aside class="tpl_contact-aside">
@@ -38,6 +38,7 @@ get_header('page');
     </form>
   </aside><!-- .tpl_contact-aside -->
 </div><!-- .tpl_contact-container -->
+<?php /*
 <div class="tpl_contact-map-container content-box margin-large grid no-padding">
   <div class="tpl_contact-map-title-container container">
     <h3 class="tpl_contact-map-title"><?php get_field('map_title'); ?></h3>
@@ -47,4 +48,4 @@ get_header('page');
     <div id="map" data-lat="<?php echo $map_data['lat']; ?>" data-lng="<?php echo $map_data['lng']; ?>" data-zoom="17"></div>
   </div><!-- .tpl_contact-map -->
 </div><!-- .tpl_contact-map-container -->
-<?php get_footer(); ?>
+<?php */get_footer(); ?>
