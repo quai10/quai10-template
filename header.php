7fb31f5061353a3e0dcd49502b152987ce9e08cb
Damien Senger <hiwelo@users.noreply.github.com>
2015-09-09 23:57:40 +0200

    Suppression d'un title
    Close #8

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="<?php bloginfo('language'); ?>">
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
-  <title><?php is_front_page() ? bloginfo('name') : wp_title(' ‐ ', true, 'right'); ?></title>
+  <!-- <title><?php is_front_page() ? bloginfo('name') : wp_title(' ‐ ', true, 'right'); ?></title> -->
   <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
 
   <link rel="stylesheet" media="all" href="<?php bloginfo('template_url'); ?>/assets/dist/css/styles.min.css">
