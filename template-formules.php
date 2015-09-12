51d58e22ddcccca09e6ded4f711f0112f743e8a7
Damien Senger <hi@hiwelo.co>
2015-09-12 17:09:28 +0200

    CSS Formules

--- template-formules.php
+++ template-formules.php
@@ -20,33 +20,38 @@ $loop = new WP_Query($args);
 
 // we display this block markup
 ?>
-<section class="tpl_formules content-box container margin-large grid no-padding-left no-padding">
-  <h2 class="tpl_formules-title content-title"><?php the_title(); ?></h2>
-  <?php if ($loop->have_posts()) : ?>
-  <ul class="tpl_formules-list grid-3">
-    <?php while ($loop->have_posts()) : $loop->the_post(); ?>
-      <li class="tpl_formules-element">
-        <h3 class="tpl_formules-element-title"><?php the_title(); ?></h3>
-        <div class="tpl_formules-element-description"><?php the_content(); ?></div>
-        <div class="tpl_formules-element-engagement">
-          <?php if (get_field('engagement')) : ?>
-            Engagement<br> <?php echo get_field('engagement'); ?>
-          <?php else: ?>
-            Sans<br> engagement
-          <?php endif; ?>
-        </div><!-- .tpl_formules-element-engagement -->
-        <div class="tpl_formules-element-price">
-          <strong><?php echo get_field('price'); ?></strong>
-          <sup>&euro; ttc</sup>
-          <em>par mois</em>
-        </div><!-- .tpl_formules-element-price -->
-        <div class="tpl_formules-element-cta-container">
-          <a class="btn" href="<?php echo get_field('cta_destination'); ?>">
-            <?php echo get_field('cta_label'); ?>
-          </a>
-        </div><!-- .tpl_formules-element-cta-container -->
-      </li>
-    <?php endwhile; ?>
-  </ul><!-- .tpl_formules-list -->
-  <?php endif; ?>
+<section class="tpl_formules content-box container margin-large grid no-padding-left">
+  <div class="tpl_formules-container grid no-padding-left no-padding">
+    <h2 class="tpl_formules-title content-title"><?php the_title(); ?></h2>
+    <?php if ($loop->have_posts()) : ?>
+    <ul class="tpl_formules-list grid-3">
+      <?php while ($loop->have_posts()) : $loop->the_post(); ?>
+        <li class="tpl_formules-element">
+          <div class="tpl_formules-element-image"><img src="<?php echo get_field('image'); ?>" alt="" title=""></div>
+          <h3 class="tpl_formules-element-title"><?php the_title(); ?></h3>
+          <div class="tpl_formules-element-description"><?php the_content(); ?></div>
+          <div class="tpl_formules-informations">
+            <div class="tpl_formules-element-engagement">
+              <?php if (get_field('engagement')) : ?>
+                Engagement<br> <?php echo get_field('engagement'); ?>
+              <?php else: ?>
+                <em>Sans<br> engagement</em>
+              <?php endif; ?>
+            </div><!-- .tpl_formules-element-engagement -->
+            <div class="tpl_formules-element-price">
+              <strong><?php echo get_field('price'); ?></strong>
+              <sup>&euro; ttc</sup>
+              <em>par mois</em>
+            </div><!-- .tpl_formules-element-price -->
+          </div><!-- .tpl_formules-informations -->
+        </li>
+      <?php endwhile; ?>
+    </ul><!-- .tpl_formules-list -->
+    <?php endif; ?>
+    <div class="tpl_formules-element-cta-container mla mra">
+      <a class="btn" href="<?php echo get_field('cta_destination'); ?>">
+        <?php echo __('Contactez-nous', 'quai10'); ?>
+      </a>
+    </div><!-- .tpl_formules-element-cta-container -->
+  </div><!-- .tpl_formules-container -->
 </section><!-- .tpl_formules -->
