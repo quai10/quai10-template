ea1dd5d9d5c772e740f6b43ff27c2f3474223157
Damien Senger <hi@hiwelo.co>
2015-09-12 10:22:17 +0200

    Frontpage header file organization

--- front-page.php
+++ front-page.php
@@ -1,31 +1,7 @@
-<?php get_header(); ?>
-  <header class="header-box container" id="top">
-    <nav class="nav-box grid-1-4">
-      <div class="nav-logo">
-        <a href="<?php echo home_url(); ?>">
-          <svg role="img" aria-labelledby="title-logo-quai10">
-            <use xlink:href="#icon-logo-quai10"></use>
-          </svg>
-        </a>
-      </div><!-- .nav-logo -->
-      <?php
-        $args = array(
-          'theme_location' => 'primary_navigation',
-          'container' => false,
-          'menu_class' => 'nav-list',
-          'menu_id' => 'navigation'
-        );
-        wp_nav_menu($args);
-        unset($args);
-      ?>
-    </nav><!-- .nav-box -->
-    <div class="header-container grid-1-4">
-      <div class="header-icon">
-        <svg role="img" aria-labelledby="title-icone-quai10"><use xlink:href="#icon-icone-quai10"></use></svg>
-      </div><!-- .header-icon -->
-      <h1 class="header-title"><?php the_title(); ?></h1>
-    </div><!-- .header-container -->
-  </header><!-- .header-box -->
+<?php
+get_header();
+get_template_part('header', 'frontpage');
+?>
   <div class="content-box container margin-large grid-2 no-padding-left">
     <?php
       $bloc_association = get_field('description_association')[0];
