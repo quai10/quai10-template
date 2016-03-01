d255d6fdf549e3dd3da2428134dd899d3e4ae745
Quai n°10 <contact@quai10.org>
2016-03-01 14:24:53 +0100

    Added 'Section' template

---
+++ template-section.php
@@ -0,0 +1,30 @@
+<?php
+/* Template Name: Section */
+
+?>
+<section class="tpl_section content-box container margin-large">
+<?php if (!empty(get_field('aside_content'))) { $grid = "grid-2-small-1"; } else {$grid = "grid";} ?>
+  <h2 class="tpl_section-title content-title"><?php the_title(); ?></h2>
+  <div class="tpl_section-container <?php echo $grid ?> no-padding-left no-padding">
+    <div>
+      <?php
+        the_content();
+      ?>
+    </div>
+    <?php if (!empty(get_field('aside_content'))) : ?>
+      <div class="tpl_section-aside" style="<?php if (!empty(get_field('aside_image'))) echo "background-image: url(" . get_field('aside_image')['url'] ." ) "; ?>">
+        <div class="tpl_section-aside-content">
+          <?php echo get_field('aside_content'); ?>
+        </div>
+        <div class="tpl_section-element-cta-container mla mra">
+        <?php if (!empty(get_field('cta_destination'))) : ?>
+          <a class="btn" href="<?php echo get_field('cta_destination'); ?>">
+            <?php echo __('Contactez-nous', 'quai10'); ?>
+          </a>
+        <?php endif; ?>
+        </div><!-- .tpl_section-element-cta-container -->
+      </div>
+    <?php endif; ?>
+    
+  </div><!-- .tpl_section-container -->
+</section><!-- .tpl_section -->
