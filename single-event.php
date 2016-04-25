bf0a980226b5885904bb25df05480979aa2d288b
Pierre Rudloff <contact@rudloff.pro>
2016-04-25 15:52:43 +0200

    Display single events correctly

--- single-event.php
+++ single-event.php
@@ -12,7 +12,11 @@
  * @link     https://quai10.org/
  */
 get_header();
-
+?>
+<section class="tpl_formules content-box container margin-large grid no-padding-left">
+<?php
 the_content();
-
+?>
+</section>
+<?php
 get_footer();
