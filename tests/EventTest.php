bbb86768c380eee5f5ee81ae4c6b84f61e1a9f0e
Pierre Rudloff <contact@rudloff.pro>
2016-08-21 22:37:37 +0200

    Use local phpunit Use Mockery to mock classes

--- tests/EventTest.php
+++ tests/EventTest.php
@@ -4,11 +4,21 @@
  */
 namespace Quai10;
 
+use Mockery;
+
 /**
  * Unit tests for Event class
  */
 class EventTest extends \PHPUnit_Framework_TestCase
 {
+
+    protected function setUp()
+    {
+        Mockery::mock('overload:EM_Events')
+            ->shouldReceive('output')
+                ->andReturn('<ul><li>Foo</li><li>Bar</li>');
+    }
+
     /**
      * Unit tests for events functions
      * @return void
