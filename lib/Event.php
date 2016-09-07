e08fba1f5f3e42b3280fd4af23779978365da876
mr-am <mr.alexandre.mathieu@gmail.com>
2016-09-07 18:24:12 +0200

    ajout lien single-event sur images

--- lib/Event.php
+++ lib/Event.php
@@ -22,7 +22,7 @@ class Event
               'format'=>'<li class="eventItem">
                   <div class="eventWrapper">
                     <div class="eventDate">#_{j M Y}</div>
-                    <div class="eventImg">#_EVENTIMAGE{372,372}</div>
+                    <div class="eventImg"><a href="#_EVENTURL">#_EVENTIMAGE{372,372}</a></div>
                     <h3 class="eventTitle"><a href="#_EVENTURL">#_EVENTNAME</a></h3>
                     <div class="eventDesc">#_EVENTNOTES</div>
                   </div>
@@ -43,7 +43,7 @@ class Event
               'format_header'=>'<ul class="eventList grid-3-medium-2-small-2-tiny-1">',
               'format'=>'<li class="eventItem">
                     <div class="eventDate">#_{j M Y}</div>
-                    <div class="eventImg">#_EVENTIMAGE{372,372}</div>
+                    <div class="eventImg"><a href="#_EVENTURL">#_EVENTIMAGE{372,372}</a></div>
                     <h3 class="eventTitle"><a href="#_EVENTURL">#_EVENTNAME</a></h3>
                     <div class="eventDesc">#_EVENTNOTES</div>
                 </li>',
