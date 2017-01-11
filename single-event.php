2f8c3ad8797cf7c495577547060edd5d2bb47335
Pierre Rudloff <contact@rudloff.pro>
2017-01-11 23:39:48 +0100

    Custom single event HTML

--- single-event.php
+++ single-event.php
@@ -16,7 +16,34 @@ get_header();
 ?>
 <section class="tpl_single-event tpl_formules content-box container margin-large grid no-padding-left">
     <div class="eventWrapper">
-        <?php the_content(); ?>
+        <?php
+        global $EM_Event;
+        echo apply_filters(
+            'em_event_output_single',
+            $EM_Event->output(
+                '<div style="float:right; margin:0px 0px 15px 15px;">#_LOCATIONMAP</div>
+                <p>
+                    <strong>Date / Heure</strong><br/>
+                    Date(s) - #_EVENTDATES<br /><i>#_EVENTTIMES</i>
+                </p>
+                {has_location}
+                <p>
+                    <strong>Emplacement</strong><br/>
+                    #_LOCATIONLINK
+                </p>
+                {/has_location}
+                <p>
+                    <strong>Catégories</strong>
+                    #_CATEGORIES
+                </p>
+                <br style="clear:both" />
+                #_EVENTNOTES',
+                'html'
+            ),
+            $EM_Event,
+            'html'
+        );
+        ?>
     </div>
 </section>
 <?php
