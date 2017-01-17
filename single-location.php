2d4c0be14f9bc2ebc6a7322bead23d0a4cc17618
Pierre Rudloff <contact@rudloff.pro>
2017-01-17 14:29:20 +0100

    Basic style on location page

---
+++ single-location.php
@@ -0,0 +1,10 @@
+<?php
+get_header();
+?>
+<section class="tpl_single-event tpl_formules content-box container margin-large grid no-padding-left">
+    <?php
+        the_content();
+    ?>
+</section>
+<?php
+get_footer();
