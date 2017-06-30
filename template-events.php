a833ac5a9284065fc8b59cbb3b70ef9c9ab5a23e
Damien Senger <damien@raccoon.studio>
2017-06-30 11:21:55 +0200

    a11y: more semantic markup and aria-labels added

--- template-events.php
+++ template-events.php
@@ -17,15 +17,15 @@ use Quai10\Event;
 
 get_header();
 ?>
-<section class="tpl_formules content-box container margin-large grid no-padding-left">
-    <div class="tpl_future-events">
-        <h2 class="tpl_formules-title">Les prochains événements</h2>
+<div class="tpl_formules content-box container margin-large grid no-padding-left">
+    <section class="tpl_future-events" aria-labelledby="a11y_nextEventsTitle">
+        <h2 class="tpl_formules-title" id="a11y_nextEventsTitle">Les prochains événements</h2>
         <div class="eventsWrapper">
             <?php
             echo Event::getFutureEvents();
             ?>
         </div>
-    </div>
+    </section><!-- .tpl_future-events -->
     <div class="eventMessage">
         <?php the_post(); ?>
         <?php the_content(); ?>
@@ -35,12 +35,12 @@ get_header();
             <p class="desc">J'ai une idée,<br>je vous la suggère !<br><a href=<?php echo get_permalink(10); ?> class="cta">&rarr;</a></p>
         </div>
     </div>
-    <div class="tpl_past-events">
-        <h2 class="tpl_formules-title">Les événements passés</h2>
+    <section class="tpl_past-events" aria-labelledby="a11y_lastEventsTitle">
+        <h2 class="tpl_formules-title" id="a11y_lastEventsTitle">Les événements passés</h2>
         <?php
         echo Event::getPastEvents();
         ?>
-    </div>
-</section>
+    </section><!-- .tpl_past-events -->
+</div>
 <?php
 get_footer();
